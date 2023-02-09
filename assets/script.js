4e720ef39d90fd3a51608dcedb92f7d26fa0baac
suturbreverof <sfriedman6711@gmail.com>
2023-02-09 11:23:00 -0700

    testing different files/scratchwork

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Variables
+
+// var
+// js is read top to bottom
+
+
+var name = "Oranges"
+
+console.log(name)
+
+var player1Score = 0;
+var player2Score = 0;
+
+console.log(player1Score)
+console.log(player2Score)
+
+console.log(
+    "Hello the score for player 1 is " +
+    player1Score +
+    " and the score for player 2 is " +
+    player2Score +
+    ".  "
+);
